4b07f6dd58b9f1ac47d03ceb2463e4c92de7edd4
bowashon <baidoofelix05@gmail.com>
2023-12-11 17:40:30 +0000

    changes made

--- models/base_model.py
+++ models/base_model.py
@@ -15,22 +15,22 @@ class BaseModel:
         Instantiate all attributes
         """
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
 
     def save(self):
         """
         update public instance attribute with current
         datetime
         """
-        self.updated_at = datetime.utcnow()
+        self.updated_at = datetime.today()
 
     def to_dict(self):
         """
         Returns dictionary containing all keys/values
         of __dict__ of the instance
         """
-        obj_dict = self.__dict__
+        obj_dict = self.__dict__.copy()
         class_name = self.__class__.__name__
         obj_dict['__class__'] = class_name
         for key, value in obj_dict.items():
