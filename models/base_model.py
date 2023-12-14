cad621f3fc9e21f8e3738c3706683fef3f509fb3
bowahson <baidoofelix05@gmail.com>
2023-12-14 12:05:05 +0000

    change made;

--- models/base_model.py
+++ models/base_model.py
@@ -19,7 +19,7 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.today()
         self.updated_at = datetime.today()
-        
+
         if kwargs:
             for key, value in kwargs.items():
                 if key != '__class__':
