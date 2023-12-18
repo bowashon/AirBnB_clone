863ba98da363eeaf715f3c70725a87cebf16d386
bowahson <baidoofelix05@gmail.com>
2023-12-18 21:29:34 +0000

    functions added

--- console.py
+++ console.py
@@ -3,6 +3,16 @@
 The console or user interface of the program
 """
 import cmd
+import re
+import shlex
+import json
+from models.base_model import BaseModel
+from models import storage
+
+
+_classes = {
+        'BaseModel': BaseModel
+        }
 
 
 class HBNBCommand(cmd.Cmd):
@@ -41,6 +51,143 @@ class HBNBCommand(cmd.Cmd):
         type help <topic> to get help on the topic
         """
         return super().do_help(arg)
+
+    def do_create(self, arg):
+        """
+        creates a new instance of BaseModel, saves it
+        to JSON file
+        **********************************************
+        Usage: create <name>
+        """
+        args = shlex.split(arg)
+        if len(args) == 0:
+            print("** class name missing **")
+
+        elif args[0] not in _classes:
+            print("** class doesn't exist **")
+        else:
+            new_obj = _classes[args[0]]()
+            new_obj.save()
+            print(new_obj.id)
+
+    def do_show(self, arg):
+        """
+        prints the string representation of an instance based on
+        the class name and id.
+        Usage : $ show BaseModel 1234-1234-1234
+        """
+        args = shlex.split(arg)
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] not in _classes:
+            print("** class doesn't exit **")
+        elif len(args) < 2:
+            print("** instance id missing **")
+        else:
+            saved_objs = storage.all()
+            key = "{}.{}".format(args[0], args[1])
+            obj = saved_objs.get(key, None)
+            if obj is None:
+                print("** no instance found **")
+            else:
+                print(obj)
+
+    def do_destroy(self, arg):
+        """
+        Deletes an instance based on the class name and id
+        Usage: destroy BaseModel 1234-1234-1234
+        """
+        args = shlex.split(arg)
+
+        if not args:
+            print("**class name missing **")
+        elif args[0] not in _classes:
+            print("** class doesn't exit **")
+        elif len(args) < 2:
+            print("** instance id missing **")
+        else:
+            key = "{}.{}".format(args[0], args[1])
+            stored_obj = storage.all()
+            obj = stored_obj.get(key, None)
+            if obj is not None:
+                del stored_obj[key]
+                storage.save()
+            else:
+                print("** no instance found **")
+                return
+
+    def do_all(self, arg):
+        """
+        Prints all string representation of all instances based
+        or not on the class name
+        Usage: all BaseModel
+               $all
+        """
+        args = shlex.split(arg)
+        all_obj = storage.all()
+
+        if not args:
+            for key, value in all_obj.items():
+                print(["{}".format(str(value))])
+                return
+
+        if args[0] not in _classes:
+            print("** class doesn't exist **")
+            return
+        else:
+            for value in all_obj.values():
+                if type(value).__name__ == args[0]:
+                    print(str(value))
+                    return
+
+    def do_update(self, arg):
+        """
+        updates instance based on the class name and id
+        by adding or updating attributes
+        Usage: <class name> <id> <attributes name> "attribute value>"
+        """
+        args = shlex.split(arg)
+
+        if not args:
+            print("** class name missing **")
+            return
+        elif args[0] not in _classes:
+            print("** class doesn't exist **")
+            return
+        elif len(args) < 2:
+            print("** instance id missing **")
+            return
+
+        all_objs = storage.all()
+        key = "{}.{}".format(args[0], args[1])
+        obj = all_objs.get(key, None)
+        if obj is None:
+            print("** no instance found **")
+            return
+
+        elif len(args) < 3:
+            print("** attribute name missing **")
+            return
+
+        elif len(args) < 4:
+            print("** value missing **")
+            return
+
+        else:
+
+            attr_name = args[2]
+            attr_value = shlex.split(args[3])[0]
+
+            if attr_name in ["id", "created_at", "updated_at"]:
+                return
+
+            try:
+                attr_value = eval(attr_value)
+            except Exception:
+                pass
+
+            setattr(obj, attr_name, attr_value)
+            obj.save()
 
 
 if __name__ == '__main__':
